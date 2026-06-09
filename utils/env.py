2d31bc131b00095411131989a46bd8bb523f10ba
Phong Ha <realphongha@gmail.com>
2026-06-09 09:01:12 +0700

    remove gpus from configs

--- utils/env.py
+++ utils/env.py
@@ -21,7 +21,7 @@ def init_cuda_cudnn(cfg):
     cudnn.enabled = cfg["cudnn"]["enabled"]
 
     os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-    if cfg["gpus"]:
+    if cfg.get("gpus", None):
         os.environ["CUDA_VISIBLE_DEVICES"] = cfg["gpus"]
     assert torch.cuda.is_available(), "CUDA is not available!"
     torch.set_float32_matmul_precision('high')
